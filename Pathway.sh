d00e6cf4bde9a4da31bbbe17b376e14ffbabb596
MyriamRAHMOUNI <54821238+MyriamRAHMOUNI@users.noreply.github.com>
2020-02-03 17:52:19 +0100

    mise en place idées de preparation

--- Pathway.sh
+++ Pathway.sh
@@ -2,8 +2,17 @@
 # cd media/bionasNFS2/GWAS/CERIES/4_IMPUTE
 #bioinfoclust01.cnam.fr 
 #QC (prunning/500kb de tout genes)  Je suis sur HG37
+#code ? zagfan1402
 
-#J'ai besoin de combien est le seuil des snp manquant en meta analyse 
+
+# donnée meta analyse? il viennent 
+# je peux utiliser les rs de chanel pour retrouver ceux des chinois ? non on va plutot utiliser 1000 genome chr par chr sans ouvrir les .gz 
+# ou sont les données des chinois ? juste devant toi ;)
+# fichier mapping celui du toto
+# c'est ok pour le serveur ld ? population oui avant je vais garder que les snp dans le fichier de mapping 
+# base de donnée pathway du tuto? non celle du lien 
+# seuil ld 0.8 ? oui 
+
 
 cd /home/mrahmouni/Myriam_2020/data_myriam_test/chanel
 
@@ -49,6 +58,44 @@ for j in list-file-relachement list-file-lentigine list-file-ride
 
 
 
+####SALIA 
+	#Creer file 1 salia rs	pval pour les 3 phenos 
+cd 
+cd home/mrahmouni/Myriam_2020/Stage2020/data_myriam/SALIA_allemand/laxity
+ls *.txt > list-laxity
+for i in `cat list-laxity` 
+	do  awk < ${i} '(NR>1){snp=$1;pvalue=$26}(NR>=1){print snp,	pvalue}'> ${i::-4}-file1
+
+cd ..
+cd lentigines 
+ls *.txt > list-lentigines
+for i in `cat list-lentigines` 
+	do  awk < ${i} '(NR>1){snp=$1;pvalue=$26}(NR>=1){print snp,	pvalue}'> ${i::-4}-file1
+
+cd ..
+cd wrinkles 
+ls *.txt > list-wrinkles
+for i in `cat list-wrinkles` 
+	do  awk < ${i} '(NR>1){snp=$1;pvalue=$26}(NR>=1){print snp,	pvalue}'> ${i::-4}-file1
+
+ls *-file1 > list
+
+### TAIZHOU 
+
+cd .. 
+cd TAIZOU_chinois
+
+awk < TAIZOU_chinois < header ' (NR==1){print CP	chr	pos	pval} (NR >= 1) {CP=$1; chr=$2; pos=$3; Laxity_Pvalue=$20} {print CP	chr	pos	Laxity_Pvalue} '> Laxity
+
+awk < TAIZOU_chinois < header ' (NR==1){print CP	chr	pos	pval} (NR >= 1) {CP=$1; chr=$2; pos=$3; Wrinkle_Pvalue=$16} {print CP	chr	pos	Wrinkle_Pvalue} '> Wrinkle
+
+awk < TAIZOU_chinois < header ' (NR==1){print CP	chr	pos	pval} (NR >= 1) {CP=$1; chr=$2; pos=$3; PigmentedSpots_Pvalue=$20} {print CP	chr	pos	PigmentedSpots_Pvalue} '> lentigines
+
+
+
+### Meta-analyses 
+
+
 #Input file 2: SNP-gene mapping : SNP	gene	dist2gene
 
 #Changer les rs en ch:pos 
@@ -64,5 +111,7 @@ awk < file.map '(NR==1) print {SNP	gene	dist2gene} (NR>=1){snp=$1;gene=$2;dist2g
 
 #Input file 3: gene-pathway : Gene Ontology (level 4), KEGG, Biocarta
 
+	#https://www.gsea-msigdb.org/gsea/msigdb/collections.jsp#C2
+	# KEGG / go biological process 
 
 #=========> Calculate_gsea.pl
